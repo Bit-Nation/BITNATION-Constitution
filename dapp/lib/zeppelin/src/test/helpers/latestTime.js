7334e46c3442eb7a3c876489dce34f0155440bb5
DeveloppSoft <developpsoft@gmail.com>
2017-08-10 17:11:13 +0200

    remove dapp in favor of BITNATION-Framework

--- dapp/lib/zeppelin/src/test/helpers/latestTime.js
+++
@@ -1,6 +0,0 @@
-import moment from 'moment'
-
-// Returns a moment.js instance representing the time of the last mined block
-export default function latestTime() {
-  return moment.unix(web3.eth.getBlock('latest').timestamp)
-}
